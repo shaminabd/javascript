d3954b645fdcf5916ae003ecaf90e9a83181bef8
shaminabd <shynggysabdullayev@gmail.com>
2023-01-11 23:06:59 +0500

    js

--- 1test.js
+++ 1test.js
@@ -1,6 +1,18 @@
-const harry = { name: 'Harry Smith', age: 42 }  
-let harrysAge = harry.age
-harry.age = 40
-harry.salary = 90000
-console.log(harry)
-console.log("Do a commit o my god")
+function biggestNumberInArray(arr) {
+    var m = -Infinity,
+      i = 0,
+      n = arr.length;
+    for (; i != n; ++i) {
+      if (arr[i] > m) {
+        m = arr[i];
+      }
+    }
+    return m;
+  }
+let b=0
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+for (i of arr) {
+    if (b<i) b=i; else b=b
+}
+console.log(b)
+console.log(biggestNumberInArray(arr))
